abdaaf912c8140434f8693e612d01fdefd21f995
Hayden <hayden@haydenzeller.com>
2024-05-25 23:53:12 -0400

    font change

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,11 @@
-import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import { Metadata } from "next";
+import { Asap as GoogleAsap } from "next/font/google"; // Renamed Asap to GoogleAsap to avoid conflict
 import "./globals.css";
 
-const inter = Inter({ subsets: ["latin"] });
+const Asap = GoogleAsap({ 
+  weight: '600',
+  subsets: ["latin"] 
+});
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -16,7 +19,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>
+      <body className={Asap.className}>
         {children}
       </body>
     </html>
--- app/page.tsx
+++ app/page.tsx
@@ -3,17 +3,16 @@ import Image from "next/image";
 
 import allscherryLogo from "../public/allscherry-logo.png"
 import SearchBar from "./components/SearchBar";
-
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col bg-base-100">
       <SearchBar/>
-      <section className="flex flex-row justify-between">
+      <section className="flex flex-row justify-center gap-16">
         <div className="ml-4 z-10 -mt-16">
           <Image priority className=" w-32" src={allscherryLogo} alt="Allscherry Logo"/>
         </div>
         <div className="flex flex-col mt-3 pr-2">
-          <h1 className="mb-4 font-bold text-sm text-center">Feel the wonder, crave adventure!</h1>
+          <h1 className="mb-4 font-bold text-md text-center">Feel the wonder, crave adventure!</h1>
           <div>
             <ul className="grid grid-cols-2 grid-rows-2 gap-7">
               <li className="flex justify-center">
@@ -40,7 +39,7 @@ export default function Home() {
           </div>
         </div>
       </section>
-      <section className="flex flex-col items-center mt-10">
+      <section className="flex flex-col items-center justify-center mt-10">
         <div className="bg-base-200 text-black rounded-3xl pt-3 pb-3 pr-8 pl-8">
           <h2 className="text-center">Sign up for our mailing list!<br/> Why? Because!</h2>
         </div>
@@ -53,7 +52,7 @@ export default function Home() {
           <button className="bg-primary mt-5 text-black p-1 border-b-accent border-b-4 rounded-2xl block w-28 text-center">Subscribe!</button>
         </div>
       </section>
-      <section>
+      <section className="flex flex-col justify-center items-center">
         <div className="text-center mt-24 text-xl">
           <h1>Here&apos;s some of our<br/>current news!
           </h1>
