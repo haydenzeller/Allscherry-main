546ad4d33da6008848c5d90434c64e21bb23fd4e
Hayden <hayden@haydenzeller.com>
2024-05-25 22:06:01 -0400

    spacing fix

--- app/components/SearchBar.tsx
+++ app/components/SearchBar.tsx
@@ -1,8 +1,12 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faSearch } from '@fortawesome/free-solid-svg-icons';
+
 export default function SearchBar() {
     return (
         <main className="flex flex-row-reverse items-center h-24 w-full bg-base-200">
-            <div className="flex w-60 mr-9">
-                <input className="text-right input input-sm bg-white rounded-3xl w-full" placeholder="Search something... anything!" />
+            <div className="flex w-64 mr-9">
+                <FontAwesomeIcon icon={faSearch}  className='w-5 m-1.5 text-black absolute'/>
+                <input className="pl-9 input input-sm bg-white rounded-3xl w-full" placeholder="Search something... anything!" />
             </div>
         </main>
     )
--- app/page.tsx
+++ app/page.tsx
@@ -8,30 +8,30 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col">
       <SearchBar/>
-      <section className="flex flex-row">
+      <section className="flex flex-row justify-between">
         <div className="ml-4 z-10 -mt-16">
           <Image className=" w-32" src={allscherryLogo} alt="Allscherry Logo"/>
         </div>
-        <div className="flex flex-col justify-end ml-3 mt-3">
-          <h1 className="mb-4 font-bold text-l text-center">Feel the wonder, crave adventure!</h1>
+        <div className="flex flex-col mt-3 pr-9">
+          <h1 className="mb-4 font-bold text-l">Feel the wonder, crave adventure!</h1>
           <div>
-            <ul className="grid grid-cols-2 grid-rows-2 gap-4">
-              <li>
+            <ul className="grid grid-cols-2 grid-rows-2 gap-7">
+              <li className="flex justify-center">
                 <Link className="bg-primary text-black p-1 border-b-accent border-b-4 rounded-3xl block w-28 text-center" href="">
                   Projects
                 </Link>
               </li>
-              <li>
+              <li className="flex justify-center">
                 <Link className="bg-primary text-black p-1 border-b-accent border-b-4 rounded-3xl block w-28 text-center" href="">
                   Shop
                 </Link>
               </li>
-              <li>
+              <li className="flex justify-center">
                 <Link className="bg-primary text-black p-1 border-b-accent border-b-4 rounded-3xl block w-28 text-center" href="">
                   Info
                 </Link>
               </li>
-              <li>
+              <li className="flex justify-center">
                 <Link className="bg-primary text-black p-1 border-b-accent border-b-4 rounded-3xl block w-28 text-center" href="">
                   Publishing
                 </Link>
