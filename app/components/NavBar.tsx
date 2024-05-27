a9416654fcadaff7d81839e156116fa72efb2262
Hayden <hayden@haydenzeller.com>
2024-05-26 20:04:49 -0400

    Tablet/laptop init

---
+++ app/components/NavBar.tsx
@@ -0,0 +1,58 @@
+import { faSearch } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import Image from "next/image";
+import allscherryLogo from "../../public/allscherry-logo.png"
+import Link from "next/link";
+
+export default function NavBar() {
+    return (
+        <section className="sticky bg-base-200 flex flex-row w-screen pt-2 justify-center items-center gap-10">
+            {/* LEFT */}
+            <div className="flex flex-col w-full">
+                <div className="flex flex-row justify-end items-end mb-2">
+                    <h1 className="text-sm text-base-100 lg:text-2xl">Feel the Wonder,</h1> 
+                </div>
+                <div>
+                    <div className="bg-base-100 h-3 w-full rounded-3xl"/>
+                </div>
+                <div className="flex flex-row-reverse pt-3 gap-14 lg:gap-24 pr-10">
+                    <Link className="bg-primary text-black p-1 border-b-accent border-b-4 rounded-2xl block w-24 lg:w-36 text-center active:translate-y-1 active:border-0" href="">
+                        Projects
+                    </Link>
+                    <Link className="bg-primary text-black p-1 border-b-accent border-b-4 rounded-2xl block w-24 lg:w-36 text-center active:translate-y-1 active:border-0" href="">
+                        Shop
+                    </Link>
+                </div>
+            </div>
+            {/* CENTER IMAGE */}
+            <div className="w-52">
+                <div className="w-24 lg:w-36 -mb-12">
+                    <Image src={allscherryLogo} alt="AllsCherry Logo"/>
+                </div>
+            </div>
+            {/* RIGHT */}
+            <div className="flex flex-col w-full">
+                <div className="flex flex-row justify-between items-center mb-2">
+                    <div className="flex flex-row">
+                        <h1 className="text-sm text-base-100 lg:text-2xl">Crave Adventure!</h1> 
+                    </div>
+                    <div className="flex w-52 lg:w-64 mr-3">
+                        <FontAwesomeIcon icon={faSearch}  className='w-5 m-1.5 text-black absolute'/>
+                        <input className=" text-black pl-9 lg:pl-8 input input-xs text-xs mr-10lg:text-s lg:input-sm bg-white rounded-3xl w-full" placeholder="Search something... anything!" />
+                    </div>
+                </div>
+                <div>
+                    <div className="bg-base-100 h-3 w-full rounded-3xl"/>
+                </div>
+                <div className="flex flex-row gap-14 lg:gap-24 pt-3 pl-10">
+                    <Link className="bg-primary text-black p-1 border-b-accent border-b-4 rounded-2xl block w-24 lg:w-36 text-center active:translate-y-1 active:border-0" href="">
+                        Publishing
+                    </Link>
+                    <Link className="bg-primary text-black p-1 border-b-accent border-b-4 rounded-2xl block w-24 lg:w-36 text-center active:translate-y-1 active:border-0" href="">
+                        Info
+                    </Link>
+                </div>
+            </div>
+        </section>
+    )
+}
--- app/page.tsx
+++ app/page.tsx
@@ -5,136 +5,144 @@ import SearchBar from "./components/SearchBar";
 import Footer from "./components/Footer";
 import SubscribeBox from "./components/SubscribeBox";
 import ScrollToTop from "./components/ScrollToTop";
+import NavBar from "./components/NavBar";
 
 export default function Home() {
   return (
     <main id="main" className="flex min-h-screen flex-col bg-base-100">
-      <SearchBar/>
-      <section className="flex flex-row justify-center gap-5">
-        <div className="ml-4 z-10 -mt-16">
-          <Image priority={true} className="w-36" src={allscherryLogo} alt="Allscherry Logo"/>
-        </div>
-        <div className="flex flex-col mt-3 pr-8">
-          <h1 className="mb-4 font-bold text-md text-center">Feel the wonder, crave adventure!</h1>
-          <div>
-            <ul className="grid grid-cols-2 grid-rows-2 gap-7">
-              <li className="flex justify-center">
-                <Link className="bg-primary text-black p-1 border-b-accent border-b-4 rounded-2xl block w-28 text-center active:translate-y-1 active:border-0" href="">
-                  Projects
-                </Link>
-              </li>
-              <li className="flex justify-center">
-                <Link className="bg-primary text-black p-1 border-b-accent border-b-4 rounded-2xl block w-28 text-center active:translate-y-1 active:border-0" href="">
-                  Shop
-                </Link>
-              </li>
-              <li className="flex justify-center">
-                <Link className="bg-primary text-black p-1 border-b-accent border-b-4 rounded-2xl block w-28 text-center active:translate-y-1 active:border-0" href="">
-                  Info
-                </Link>
-              </li>
-              <li className="flex justify-center">
-                <Link className="bg-primary text-black p-1 border-b-accent border-b-4 rounded-2xl block w-28 text-center active:translate-y-1 active:border-0" href="">
-                  Publishing
-                </Link>
-              </li>
-            </ul>
+      {/* MOBILE */}
+      <section className="md:hidden">
+        <SearchBar/>
+        <section className="flex flex-row justify-center gap-5">
+          <div className="ml-4 z-10 -mt-16">
+            <Image priority={true} className="w-36" src={allscherryLogo} alt="Allscherry Logo"/>
           </div>
-        </div>
-      </section>
-      <section className="flex flex-col items-center justify-center mt-14">
-        <div className="bg-base-300 text-black rounded-2xl absolute pt-3 pb-3 pr-8 pl-8 h-[60px] w-[270px] mb-[110px] z-0" />
-        <div className="flex items-center justify-center bg-base-200 text-black rounded-2xl pt-3 pb-3 pr-8 pl-8 relative z-10">
-          <h2 className="text-center">
-            Sign up for our mailing list!
-            <br /> Why? Because!
-          </h2>
-        </div>
-        <SubscribeBox />
-      </section>
-      <section className="flex flex-col justify-center mt-14 items-center">
-        <div className="text-center text-xl">
-          <h1>Here&apos;s some of our<br/>current news!
-          </h1>
-        </div>
-        <div className="carousel carousel-center max-w-md p-4 space-x-4 bg-base-100 rounded-box">
-          <div className="carousel-item">
-            <img src="https://img.daisyui.com/images/stock/photo-1559703248-dcaaec9fab78.jpg" className="rounded-box" />
-          </div> 
-          <div className="carousel-item">
-            <img src="https://img.daisyui.com/images/stock/photo-1565098772267-60af42b81ef2.jpg" className="rounded-box" />
-          </div> 
-          <div className="carousel-item">
-            <img src="https://img.daisyui.com/images/stock/photo-1572635148818-ef6fd45eb394.jpg" className="rounded-box" />
-          </div> 
-          <div className="carousel-item">
-            <img src="https://img.daisyui.com/images/stock/photo-1494253109108-2e30c049369b.jpg" className="rounded-box" />
-          </div> 
-          <div className="carousel-item">
-            <img src="https://img.daisyui.com/images/stock/photo-1550258987-190a2d41a8ba.jpg" className="rounded-box" />
-          </div> 
-          <div className="carousel-item">
-            <img src="https://img.daisyui.com/images/stock/photo-1559181567-c3190ca9959b.jpg" className="rounded-box" />
-          </div> 
-          <div className="carousel-item">
-            <img src="https://img.daisyui.com/images/stock/photo-1601004890684-d8cbf643f5f2.jpg" className="rounded-box" />
+          <div className="flex flex-col mt-3 pr-8">
+            <h1 className="mb-4 font-bold text-md text-center">Feel the wonder, crave adventure!</h1>
+            <div>
+              <ul className="grid grid-cols-2 grid-rows-2 gap-7">
+                <li className="flex justify-center">
+                  <Link className="bg-primary text-black p-1 border-b-accent border-b-4 rounded-2xl block w-28 text-center active:translate-y-1 active:border-0" href="">
+                    Projects
+                  </Link>
+                </li>
+                <li className="flex justify-center">
+                  <Link className="bg-primary text-black p-1 border-b-accent border-b-4 rounded-2xl block w-28 text-center active:translate-y-1 active:border-0" href="">
+                    Shop
+                  </Link>
+                </li>
+                <li className="flex justify-center">
+                  <Link className="bg-primary text-black p-1 border-b-accent border-b-4 rounded-2xl block w-28 text-center active:translate-y-1 active:border-0" href="">
+                    Info
+                  </Link>
+                </li>
+                <li className="flex justify-center">
+                  <Link className="bg-primary text-black p-1 border-b-accent border-b-4 rounded-2xl block w-28 text-center active:translate-y-1 active:border-0" href="">
+                    Publishing
+                  </Link>
+                </li>
+              </ul>
+            </div>
           </div>
-        </div>
-      </section>
-      <section className="flex flex-col justify-center items-center mt-14">
-        <div className="mb-5">
-          <h1 className="text-center text-xl">What even is an<br/><i>Allscherry</i>?<br/>Good question!</h1>
-        </div>
-        <div className="flex flex-col justify-center items-center">
-          <div className="bg-base-300 text-black rounded-2xl absolute h-[90px] w-[340px] mt-[50px] z-0" />
+        </section>
+        <section className="flex flex-col items-center justify-center mt-14">
+          <div className="bg-base-300 text-black rounded-2xl absolute pt-3 pb-3 pr-8 pl-8 h-[60px] w-[270px] mb-[110px] z-0" />
           <div className="flex items-center justify-center bg-base-200 text-black rounded-2xl pt-3 pb-3 pr-8 pl-8 relative z-10">
             <h2 className="text-center">
-              AllsCherry, is a brand that represents<br/>community, quality stories, and<br/>collabrative art pieces of all kinds!
+              Sign up for our mailing list!
+              <br /> Why? Because!
             </h2>
           </div>
-        </div>
-        <div className="flex flex-col justify-center items-center mt-14">
-          <div className="bg-base-300 text-black rounded-2xl absolute h-[90px] w-[360px] mt-[50px] z-0" />
-          <div className="flex items-center justify-center bg-base-200 text-black rounded-2xl pt-3 pb-3 pr-8 pl-8 relative z-10">
-            <h2 className="text-center">
-              Sound intriguing? Wanna join in? Take a<br/>look around the site, or shoot an email<br/>to the founder, Eric!
-            </h2>
+          <SubscribeBox />
+        </section>
+        <section className="flex flex-col justify-center mt-14 items-center">
+          <div className="text-center text-xl">
+            <h1>Here&apos;s some of our<br/>current news!
+            </h1>
           </div>
-        </div>
+          <div className="carousel carousel-center max-w-md p-4 space-x-4 bg-base-100 rounded-box">
+            <div className="carousel-item">
+              <img src="https://img.daisyui.com/images/stock/photo-1559703248-dcaaec9fab78.jpg" className="rounded-box" />
+            </div> 
+            <div className="carousel-item">
+              <img src="https://img.daisyui.com/images/stock/photo-1565098772267-60af42b81ef2.jpg" className="rounded-box" />
+            </div> 
+            <div className="carousel-item">
+              <img src="https://img.daisyui.com/images/stock/photo-1572635148818-ef6fd45eb394.jpg" className="rounded-box" />
+            </div> 
+            <div className="carousel-item">
+              <img src="https://img.daisyui.com/images/stock/photo-1494253109108-2e30c049369b.jpg" className="rounded-box" />
+            </div> 
+            <div className="carousel-item">
+              <img src="https://img.daisyui.com/images/stock/photo-1550258987-190a2d41a8ba.jpg" className="rounded-box" />
+            </div> 
+            <div className="carousel-item">
+              <img src="https://img.daisyui.com/images/stock/photo-1559181567-c3190ca9959b.jpg" className="rounded-box" />
+            </div> 
+            <div className="carousel-item">
+              <img src="https://img.daisyui.com/images/stock/photo-1601004890684-d8cbf643f5f2.jpg" className="rounded-box" />
+            </div>
+          </div>
+        </section>
+        <section className="flex flex-col justify-center items-center mt-14">
+          <div className="mb-5">
+            <h1 className="text-center text-xl">What even is an<br/><i>Allscherry</i>?<br/>Good question!</h1>
+          </div>
+          <div className="flex flex-col justify-center items-center">
+            <div className="bg-base-300 text-black rounded-2xl absolute h-[90px] w-[340px] mt-[50px] z-0" />
+            <div className="flex items-center justify-center bg-base-200 text-black rounded-2xl pt-3 pb-3 pr-8 pl-8 relative z-10">
+              <h2 className="text-center">
+                AllsCherry, is a brand that represents<br/>community, quality stories, and<br/>collabrative art pieces of all kinds!
+              </h2>
+            </div>
+          </div>
+          <div className="flex flex-col justify-center items-center mt-14">
+            <div className="bg-base-300 text-black rounded-2xl absolute h-[90px] w-[360px] mt-[50px] z-0" />
+            <div className="flex items-center justify-center bg-base-200 text-black rounded-2xl pt-3 pb-3 pr-8 pl-8 relative z-10">
+              <h2 className="text-center">
+                Sound intriguing? Wanna join in? Take a<br/>look around the site, or shoot an email<br/>to the founder, Eric!
+              </h2>
+            </div>
+          </div>
+        </section>
+        <section className="flex flex-col items-center justify-center mt-24">
+          <div>
+            <h1 className="text-xl text-center">Our featured projects</h1>
+          </div>
+          <div className="grid grid-cols-2 grid-rows-3 gap-24 mt-9 text-center">
+            <div>
+              {/* <Image /> */}
+              <p>Wendy and the Sprite</p>
+            </div>
+            <div>
+              {/* <Image /> */}
+              <p>Overflow</p>
+            </div>
+            <div>
+              {/* <Image /> */}
+              <p>AllsCherry Anthology</p>
+            </div>
+            <div>
+              {/* <Image /> */}
+              <p>Printing and Publishing</p>
+            </div>
+            <div>
+              {/* <Image /> */}
+              <p>Prophets of Grey</p>
+            </div>
+            <div>
+              {/* <Image /> */}
+              <p>Crow on a Perch</p>
+            </div>
+          </div>
+        </section>
+        <ScrollToTop />
+        <Footer />
       </section>
-      <section className="flex flex-col items-center justify-center mt-24">
-        <div>
-          <h1 className="text-xl text-center">Our featured projects</h1>
-        </div>
-        <div className="grid grid-cols-2 grid-rows-3 gap-24 mt-9 text-center">
-          <div>
-            {/* <Image /> */}
-            <p>Wendy and the Sprite</p>
-          </div>
-          <div>
-            {/* <Image /> */}
-            <p>Overflow</p>
-          </div>
-          <div>
-            {/* <Image /> */}
-            <p>AllsCherry Anthology</p>
-          </div>
-          <div>
-            {/* <Image /> */}
-            <p>Printing and Publishing</p>
-          </div>
-          <div>
-            {/* <Image /> */}
-            <p>Prophets of Grey</p>
-          </div>
-          <div>
-            {/* <Image /> */}
-            <p>Crow on a Perch</p>
-          </div>
-        </div>
+      {/* TABLET || SMALL LAPTOP */}
+      <section className="hidden md:flex md:flex-col md:justify-center md:items-center">
+        <NavBar/>
       </section>
-      <ScrollToTop />
-      <Footer />
     </main>
   );
 }
