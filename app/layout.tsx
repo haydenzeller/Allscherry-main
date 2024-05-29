44f478cd46a47a1111142ff8a75d193afefad08d
Hayden <hayden@haydenzeller.com>
2024-05-28 23:53:26 -0400

    favicons

--- app/layout.tsx
+++ app/layout.tsx
@@ -19,6 +19,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <link rel="icon" href="/favicon.svg" sizes="any" type="image/svg+xml" />
+      </head>
       <body className={Asap.className}>
         {children}
       </body>
