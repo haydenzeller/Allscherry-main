278fd7e73f51b0fb2064a2bc5b7911b7dc7352bb
Hayden <hayden@haydenzeller.com>
2024-06-04 11:24:23 -0400

    meta

--- app/layout.tsx
+++ app/layout.tsx
@@ -29,8 +29,16 @@ export default function RootLayout({
   return (
     <html lang="en" className="overflow-x-hidden">
       <head>
-
-        <link rel="icon" href="/favicon.svg" sizes="any" type="image/svg+xml" />
+        <link rel="icon" href="/icon.svg" sizes="any" type="image/svg+xml" />
+        <meta name="theme-color" content="#FFB7B7" />
+        <meta name="description" content="AllsCherry" />
+        <meta property="og:title" content="AllsCherry | Home" />
+        <meta property="og:description" content="AllsCherry" />
+        <meta property="og:image" content="/icon.svg" />
+        <meta property="og:url" content="https://allscherry.com" />
+        <meta name="twitter:title" content="AllsCherry | Home" />
+        <meta name="twitter:description" content="AllsCherry | Home" />
+        <meta name="twitter:image" content="/icon.svg" />
       </head>
       <body className={Asap.className}>
         {children}
