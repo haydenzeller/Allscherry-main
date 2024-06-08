04e07090b543c3967b459a8ab4288bba5d6650dd
Hayden <hayden@haydenzeller.com>
2024-06-07 23:37:59 -0400

    remove non next meta

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,3 @@
-import { Metadata } from "next";
 import { Asap as GoogleAsap } from "next/font/google"; // Renamed Asap to GoogleAsap to avoid conflict
 import "./globals.css";
 import { Viewport } from "next";
@@ -19,8 +18,6 @@ export const viewport: Viewport = {
   themeColor:"#FFB7B7",
 };
 
-
-
 export default function RootLayout({
   children,
 }: Readonly<{
@@ -29,16 +26,6 @@ export default function RootLayout({
   return (
     <html lang="en" className="overflow-x-hidden">
       <head>
-        <link rel="icon" href="/icon.svg" sizes="any" type="image/svg+xml" />
-        <meta name="description" content="AllsCherry" />
-        <meta name="color-scheme" content="light"></meta>
-        <meta property="og:title" content="AllsCherry | Home" />
-        <meta property="og:description" content="AllsCherry" />
-        <meta property="og:image" content="/icon.svg" />
-        <meta property="og:url" content="https://allscherry.com" />
-        <meta name="twitter:title" content="AllsCherry | Home" />
-        <meta name="twitter:description" content="AllsCherry | Home" />
-        <meta name="twitter:image" content="/icon.svg" />
       </head>
       <GoogleAnalytics/>
       <body className={Asap.className}>
