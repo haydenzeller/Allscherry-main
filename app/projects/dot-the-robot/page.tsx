c6b86971eddb52a51bf032d714101f4c09e2b0c0
Hayden Zeller <hayden@haydenzeller.com>
2024-07-03 00:25:43 -0400

    Update

--- app/projects/dot-the-robot/page.tsx
+++ app/projects/dot-the-robot/page.tsx
@@ -6,6 +6,13 @@ import dotmntgenesis from "@/public/dot-mnt-genesis.jpg";
 import dotmodel from "@/public/dot-model.png";
 import dotpage1 from "@/public/dot-page1.jpg";
 import Link from "next/link";
+import dot8 from "@/public/dot8.jpeg";
+import dot2 from "@/public/dot2.jpeg";
+import dot3 from "@/public/dot3.jpeg";
+import dot4 from "@/public/dot4.jpeg";
+import dot5 from "@/public/dot5.jpeg";
+import dot6 from "@/public/dot6.jpeg";
+import dot7 from "@/public/dot7.jpeg";
 
 export const metadata: Metadata = {
     title: "AllsCherry | Dot the Robot",
@@ -45,7 +52,7 @@ export default function DotTheRobot() {
                         <Image src={dotmntgenesis} alt="Dot the Robot Mount Genesis" className="rounded-3xl"/>                        
                     </div>
                     <div className="flex flex-col items-center justify-center relative">
-                        <h1 className="border border-base-200 rounded-3xl p-3 w-5/6 mt-6 text-center">Dot the Robot, the “beginning” of it all. Originally inspir4ed during his trip to Victoria, BC in 2023, Eric was experimenting with many new ideas as well as a whole new lifestyle.<br/><br/>In his personal life, a lot had changed and for the better. This not only changed a a lifestyle, but the art style too!</h1>
+                        <h2 className="border border-base-200 rounded-3xl p-3 w-5/6 mt-6 text-center">Dot the Robot, the “beginning” of it all. Originally inspir4ed during his trip to Victoria, BC in 2023, Eric was experimenting with many new ideas as well as a whole new lifestyle.<br/><br/>In his personal life, a lot had changed and for the better. This not only changed a a lifestyle, but the art style too!</h2>
                     </div>
                     <div className="flex flex-col items-center justify-center relative mx-5 my-12">
                         <Image src={dotpage1} alt="Dot the Robot Page 1" className="rounded-3xl"/>
@@ -61,32 +68,32 @@ export default function DotTheRobot() {
                     </div>
                 </section>
                 <section className="flex flex-col items-center justify-center">
-                    <div className="grid grid-cols-2 grid-rows-3 gap-1 mt-6">
+                    <div className="grid grid-cols-2 grid-rows-3 gap-3 mt-6">
                         <div className="flex flex-col items-center justify-center relative">
-                            <Image src={dotmntgenesis} alt="Dot the Robot Mount Genesis" className="rounded-3xl"/>
+                            <Image src={dot2} alt="Dot the Robot Mount Genesis" className="rounded-3xl"/>
                         </div>
                         <div className="flex flex-col items-center justify-center relative">
-                            <Image src={dotmntgenesis} alt="Dot the Robot Mount Genesis" className="rounded-3xl"/>
+                            <Image src={dot3} alt="Dot the Robot Mount Genesis" className="rounded-3xl"/>
                         </div>
                         <div className="flex flex-col items-center justify-center relative">
-                            <Image src={dotmntgenesis} alt="Dot the Robot Mount Genesis" className="rounded-3xl"/>
+                            <Image src={dot4} alt="Dot the Robot Mount Genesis" className="rounded-3xl"/>
                         </div>
                         <div className="flex flex-col items-center justify-center relative">
-                            <Image src={dotmntgenesis} alt="Dot the Robot Mount Genesis" className="rounded-3xl"/>
+                            <Image src={dot5} alt="Dot the Robot Mount Genesis" className="rounded-3xl"/>
                         </div>
                         <div className="flex flex-col items-center justify-center relative">
-                            <Image src={dotmntgenesis} alt="Dot the Robot Mount Genesis" className="rounded-3xl"/>
+                            <Image src={dot6} alt="Dot the Robot Mount Genesis" className="rounded-3xl"/>
                         </div>
                         <div className="flex flex-col items-center justify-center relative">
-                            <Image src={dotmntgenesis} alt="Dot the Robot Mount Genesis" className="rounded-3xl"/>
+                            <Image src={dot7} alt="Dot the Robot Mount Genesis" className="rounded-3xl"/>
                         </div>
                     </div>
-                    <div className="flex flex-col items-center justify-center relative">
-                        <Image src={dotmntgenesis} alt="Dot the Robot Mount Genesis" className="rounded-3xl"/>
+                    <div className="flex flex-col items-center justify-center relative mt-6">
+                        <Image src={dot8} alt="Dot the Robot Mount Genesis" className="rounded-3xl"/>
                     </div>
                     <div className="flex flex-col items-center justify-center">
                         <h2 className="border border-base-200 rounded-3xl p-3 w-5/6 mt-6 text-center">But of course, Eric&apos;s ideas did not stop there. Because now that we have seen the past of this “Grey Planet” Wendy and the gang traverse, what about the past&apos;s past??<br/><br/>Always wanting to dabble into music, since all of his comics stemmed from musical inspiration, the idea to make an album sprouted from this on-going project. Where did the world of Dot the Robot originate?</h2>
-                        <Link href="/projects/wendy-and-the-sprite" className="bg-primary text-black p-2 mt-4 border-b-accent border-b-4 rounded-full block w-34 text-center active:mt-5 active:border-0">Prophets of Grey</Link>
+                        <Link href="/projects/prophets-of-grey" className="bg-primary text-black p-2 mt-4 border-b-accent border-b-4 rounded-full block w-34 text-center active:mt-5 active:border-0">Prophets of Grey</Link>
                     </div>
                 </section>
                 <Footer />
--- app/projects/prophets-of-grey/page.tsx
+++ app/projects/prophets-of-grey/page.tsx
@@ -1,4 +1,6 @@
 import Image from 'next/image';
+import Header from '@/app/components/Header';
+import Footer from '@/app/components/Footer';
 import Link from 'next/link';
 
 
@@ -6,23 +8,25 @@ import Link from 'next/link';
 export default function ProphetsOfGrey() {
     return (
         <main className="flex flex-col min-h-screen bg-base-100 lg:hidden">
-            <section>
+            <Header/>
+            <section className="flex flex-col bg-base-100 items-center justify-center">
                 <div className="">
                     <Image src="" alt=""/>
                 </div>
-                <h2 className="border border-base-200 rounded-full p-3 w-5/6 mt-6 text-center">The Prophets of Grey, a project Eric debated for a very long time, is the name of his three piece band. Consisting of the Three Prophets of the Grey Planet</h2>
+                <h2 className="border border-base-200 mt-6 rounded-3xl p-3 w-5/6 text-center">The Prophets of Grey, a project Eric debated for a very long time, is the name of his three piece band. Consisting of the Three Prophets of the Grey Planet</h2>
                 <div>
                     <Image src="" alt=""/>
                 </div>
-                <h2 className="border border-base-200 rounded-full p-3 w-5/6 mt-6 text-center">The first of many more possible albums, is A Split in the Sun, consisting of 8 songs for a total of 1 hour and 21 minutes. Eric grew up as a metal head, slowly drifting to jazz and avant garde music such as King Crimson, Frank Zappa and Mr. Bungle. The craving to create music only grew as his ideas for comics did too. After Dot’s story was finished being written, he asked himself, but what happeed before Dot the Robot?</h2>
+                <h2 className="border border-base-200 rounded-3xl p-3 w-5/6 mt-6 text-center">The first of many more possible albums, is A Split in the Sun, consisting of 8 songs for a total of 1 hour and 21 minutes. Eric grew up as a metal head, slowly drifting to jazz and avant garde music such as King Crimson, Frank Zappa and Mr. Bungle. The craving to create music only grew as his ideas for comics did too. After Dot’s story was finished being written, he asked himself, but what happeed before Dot the Robot?</h2>
                 <div>
                     <Image src="" alt=""/>
                     <Image src="" alt=""/>
                     <Image src="" alt=""/>
                     <Image src="" alt=""/>
                 </div>
-                <h2 className="border border-base-200 rounded-full p-3 w-5/6 mt-6 text-center">Although the music is finished, a lot goes into making an album! Stay tuned for the rest of this page to be finished:)</h2>
+                <h2 className="border border-base-200 rounded-3xl p-3 w-5/6 mt-6 text-center">Although the music is finished, a lot goes into making an album! Stay tuned for the rest of this page to be finished:)</h2>
             </section>
+            <Footer/>
         </main>
     );
 }
