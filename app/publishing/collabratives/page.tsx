a791e97cdd6cba22ab7cb53024c442cbdd46a775
Hayden Zeller <hayden@haydenzeller.com>
2024-07-06 12:22:35 -0400

    publishing rework

---
+++ app/publishing/collabratives/page.tsx
@@ -0,0 +1,9 @@
+export default function Collabratives() {
+    return (
+        <>
+            <section className="flex flex-col items-center justify-center">
+                <
+            </section>
+        </>
+    );
+}
--- app/publishing/page.tsx
+++ app/publishing/page.tsx
@@ -1,15 +1,17 @@
-import PageTitle from "@/app/components/PageTitle";
 import Header from "@/app/components/Header";
 import Footer from "@/app/components/Footer";
+import PageTitle from "@/app/components/PageTitle";
+
 import Image from "next/image";
 import goingUp from "@/public/going-up-arrow.svg"
 import Link from "next/link";
 import mailLogo from "@/public/mail.svg"
 import indieSign from "@/public/indie-sign-square.jpg"
 import comicsPublishing from "@/public/allscherry-comics-publishing.jpg"
-import prints from "@/public/print-carousel.jpg"
-import underConstruction from "@/public/underConstruction.svg"
 import { Metadata } from "next";
+import anthCarousel from "@/public/anthCarousel4.jpg";
+import studioComix from "@/public/print-carousel.jpg";
+
 
 export const metadata: Metadata = {
     title: "AllsCherry | Publishing",
@@ -38,89 +40,86 @@ export const metadata: Metadata = {
         ],
     },
 };
-export default function Publishing() {
+
+export default function Publushing() {
     return (
-        <>
-            <main className="flex min-h-screen flex-col bg-base-100 lg:hidden">
-                <Header />
-                <PageTitle title="AllsCherry is primarily a comic book brand!"/>
-                <section className="flex flex-col justify-center items-center text-center mx-3 mt-14 gap-4">
-                    <div className="flex flex-row items-center justify-center">
-                        <div>
+        <main>
+            <section className="flex flex-col min-h-screen bg-base-100">
+                <Header/>
+                <PageTitle title={"AllsCherry is an Indie Publishing Company!"}/>
+                <div className="flex flex-col items-center justify-center mt-6">
+                    <h2 className="text-md w-10/12 text-center border border-base-200 rounded-3xl p-3">What Separates us from the rest?<br/><br/>Community, passion, and artistic freedom!</h2>
+                </div>
+                <section className="flex bg-base-200 flex-col items-center justify-center mt-6 mx-5 rounded-3xl">
+                    <div className="rounded-3xl text-black bg-[#FFDBDB] p-3 px-5 m-3">
+                        <h2>AllsCherry has 3 core values...</h2>
+                    </div>
+                    <div className="mt-2">
+                        <div className="rounded-3xl text-black bg-white p-3 px-5 m-3">
+                            <h2>1. Tyranny is not Art</h2>
+                        </div>
+                        <div className=" rounded-3xl text-black p-3 px-5 m-3 bg-[#FFDBDB]">
+                            <h2>Have an idea? Let’s make it happen! Your way</h2>
+                        </div>
+                    </div>
+                    <div className="mt-2">
+                        <div className="rounded-3xl text-black bg-white p-3 px-5 m-3">
+                            <h2>2. A strong community builds strong projects</h2>
+                        </div>
+                        <div className="rounded-3xl text-black p-3 px-5 m-3 bg-[#FFDBDB]">
+                            <h2>AllsCherry works with many types of artists; building teams for projects or for collaborative works!</h2>
+                        </div>
+                    </div>             
+                    <div className="mt-2">
+                        <div className="rounded-3xl text-black bg-white p-3 px-5 m-3">
+                            <h2>3. Origiality is key</h2>
+                        </div>
+                        <div className="rounded-3xl text-black p-3 px-5 m-3 bg-[#FFDBDB]">
+                            <h2>Creating something new, and truly yours, is what makes for something truly special!</h2>
+                        </div>
+                    </div>
+                </section>
+                <section className="flex flex-col justify-center items-center text-center mx-1 mt-14 gap-5">
+                    <div className="flex flex-row items-center justify-center gap-3">
+                        <div className="w-1/2 flex flex-col justify-center">
                             <Image src={comicsPublishing} alt="Comics Publishing" width={700} height={700} className="rounded-box"/>
+                            <Link href="/publishing/anthology" className="bg-primary text-black p-2 px-1 mt-4 border-b-accent border-b-4 rounded-full block w-34 mx-1 text-center active:mt-5 active:border-0">Let&apos;s make a book!</Link>
                         </div>
-                        <div className="flex justify-center">
-                            <h1 className="text-md w-10/12 text-center border border-base-200 rounded-3xl p-3">AllsCherry has it&apos;s own label, used to collaborate with artists, distribute and sell it&apos;s books while staying independent and local!</h1>
-                        </div>
-                    </div>
-                    <div className="flex flex-row-reverse items-center justify-center">
-                        <div>
-                            <Image src={indieSign} alt="Indie Sign" width={700} height={700} className="rounded-box"/>
-                        </div>
-                        <div className="flex justify-center">
-                            <p className="text-md w-10/12 text-center border border-base-200 rounded-3xl p-3">We strive to keep the artist&apos;s work their own, leaving the creativity to them, and the heavy lifting for us!</p>
+                        <div className="flex justify-center w-1/2">
+                            <h2 className="text-md text-center border border-base-200 rounded-3xl p-3">AllsCherry believes if someone tells you how to make your art, then it&apos;s not your art!<br/><br/>We make your ideas come to life how you want it. Our CEO and Chief Editor personally bring you from Step 0 to a book in hand with our Publication Service!</h2>
                         </div>
                     </div>
-                </section>
-                <section className="flex flex-col items-center justify-center text-center m-10">
-                    <h2 className="text-md w-10/12 text-center border border-base-200 rounded-3xl p-3">Want to publish yourself, but don&apos;t have a printer? Need some business cards? A poster? Brochures?!<br/><br/>Don&apos;t worry, we know a guy ;)</h2>
-                    <Image className="mt-10 rounded-box" src={prints} alt="Printing" width={300} height={300}/>
-                    <Image src={goingUp} alt="Going Up?" className="w-12 filter grayscale rotate-90 "></Image>
-                </section>
-                <section className="flex flex-col justify-center items-center text-center mr-14 ml-14">
-                    <h2 className="text-md w-10/12 text-center border border-base-200 rounded-3xl p-3">We print with a local printer, StudioComix Press, located in Kitchener, Ontario!<br/><br/>The Process is simple!<br/><br/>Send us an email with these requirements in mind...</h2>
-                </section>
-                <section className="flex flex-col justify-center items-center mb-14 mt-14">
-                    <div className="relative flex items-center justify-center mr-4 ml-4 text-black rounded-2xl">
-                    <div className="absolute -inset-2 z-0 bg-base-300 rounded-2xl transform scale-105 mt-24"></div>
-                        <div className="text-center text-xl relative z-10 bg-base-200 rounded-2xl py-5 px-10">
-                            <ul>
-                                <li className="list-disc text-left text-md">What is the item? (Business cards, graphic novel, poster, etc...)<br/><br/></li>
-                                <li className="list-disc text-left text-md">How many pages is your item in spread format (double sided!)<br/><br/></li>
-                                <li className="list-disc text-left text-md">Is it in colour or black and white?<br/><br/></li>
-                                <li className="list-disc text-left text-md">What is the item? (Business cards, graphic novel, poster, etc...)<br/><br/></li>
-                                <li className="list-disc text-left text-md">How many units do you need? (Ex. 100 units, 200 units, etc.)<br/><br/></li>
-                                <li className="list-disc text-left text-md">What are the dimensions of your item? (Ex. 8.5” x 11”)<br/><br/></li>
-                            </ul>
+                    <div className="flex flex-row-reverse items-center justify-center gap-3">
+                        <div className="w-1/2 flex flex-col justify-center">
+                            <Image src={anthCarousel} alt="Comics Publishing" width={700} height={700} className="rounded-box"/>
+                            <Link href="/publishing/anthology" className="bg-primary text-black p-2 px-1 mt-4 border-b-accent border-b-4 rounded-full block w-34 mx-1 text-center active:mt-5 active:border-0">Be included here!</Link>
+                        </div>
+                        <div className="flex justify-center w-1/2">
+                            <h2 className="text-md text-center border border-base-200 rounded-3xl p-3">AllsCherry continuously builds it&apos;s community to include all kinds of creative minds.<br/><br/>We pride ourselves in our collaborative Annual Anthology and Stubby Stories books to bring together beginner and veteran artists!</h2>
+                        </div>
+                    </div>
+                    <div className="flex flex-row items-center justify-center gap-3">
+                        <div className="w-1/2 flex flex-col justify-center">
+                            <Image src={indieSign} alt="Comics Publishing" width={700} height={700} className="rounded-box"/>
+                            <Link href="/publishing/anthology" className="bg-primary text-black p-2 px-1 mt-4 border-b-accent border-b-4 rounded-full block w-34 mx-1 text-center active:mt-5 active:border-0">Let&apos;s hear it!</Link>
+                        </div>
+                        <div className="flex justify-center w-1/2">
+                            <h2 className="text-md text-center border border-base-200 rounded-3xl p-3">Originality in art is a core aspect at AllsCherry, not only for the sake of the artists here, but for the audience too!<br/><br/>Everybody loves a new and captivating story, so let&apos;s help you plan yours out, we&apos;d love to hear it!</h2>
                         </div>
                     </div>
                 </section>
-                <section className="flex flex-col justify-center mt-14 items-center">
-                    <div className="flex flex-col items-center justify-center mt-5">
-                        <Link href="/info/contact#contactForm" className="gap-3 flex flex-row items-center justify-center text-xl">
-                        <Image className="bg-white rounded-full p-1" src={mailLogo} alt="Email Logo" width="50"/>
-                        Email us for a quote!
-                        </Link>
-                        <div className="h-3 my-5 w-full ">
-                            <div className="bg-white h-full w-full rounded-3xl"></div>
-                        </div>
+                <section className="flex flex-col justify-center items-center text-center mx-1 mt-14 gap-5">
+                    <div className="flex flex-col items-center justify-center mx-1">
+                        <h2 className="text-md w-10/12 text-center border border-base-200 rounded-3xl p-3">AllsCherry is a proud Ambassador of Canadian Printer and Comic Shop, StudioComix Press!</h2>
+                        <Image src={studioComix} alt="StudioComix" className="rounded-3xl w-3/4 mt-6"/>
                     </div>
-                </section>
-                <section className="flex flex-col items-center justify-center text-center mt-14">
-                    <div className="flex justify-center">
-                        <h2 className="text-lg w-3/4 text-center border border-base-200 rounded-3xl p-3">Don&apos;t know where to start? We can help you design something!<br/><br/>Or maybe you want to start in one of our Anthologies?</h2>
-                    </div>
-                    <div className="flex flex-row mt-14 mb-14 gap-10">
-                        <div>
-                            <Link className="bg-primary text-black p-1 border-b-accent border-b-4 rounded-2xl block w-28 text-center active:mt-1 active:border-0" href="/publishing/anthology">
-                                Join in!
-                            </Link>
-                        </div>
-                        <div>
-                            <Link className="bg-primary text-black p-1 border-b-accent border-b-4 rounded-2xl block w-28 text-center active:mt-1 active:border-0" href="/publishing/design">
-                                Design Help
-                            </Link>
-                        </div>
+                    <div className="flex flex-col items-center justify-center mx-1 mt-6">
+                        <h2 className="text-md w-10/12 text-center border border-base-200 rounded-3xl p-3">Don’t know where to start? Check out our application form, or simply email us! We’re happy to help!</h2>
+                        <Link href="/info/contact" className="bg-primary text-black p-2 px-14 mt-6 border-b-accent border-b-4 rounded-full block w-34 text-center active:mt-5 active:border-0">Apply Here:)</Link>
                     </div>
                 </section>
                 <Footer/>
-            </main>
-            <main className="hidden lg:flex flex-col items-center justify-center min-h-screen">
-                <div className="flex flex-col items-center justify-center">
-                    <Image src={underConstruction} alt="Under Construction" width={400} height={400}/>
-                    <h1 className="text-2xl w-1/2 text-center">Oop, sorry! Desktop version is under construction, check back later!</h1>
-                </div>
-            </main>
-        </>
-    )
+            </section>
+        </main>
+    );
 }
