b357e84ba3ea167b32d556d201d4f8fddfd82f80
Hayden Zeller <hayden@haydenzeller.com>
2024-07-06 16:22:46 -0400

    team update

--- app/info/affiliates/page.tsx
+++ app/info/affiliates/page.tsx
@@ -5,7 +5,8 @@ import Footer from "@/app/components/Footer";
 import Image from "next/image";
 import underConstruction from "@/public/underConstruction.svg";
 import { Metadata } from "next";
-
+import Link from "next/link";
+import mailLogo from "@/public/mail.svg";
 export const metadata: Metadata = {
     title: "AllsCherry | Affiliates",
     description: "AllsCherry Affiliates Page - Who helps out around AllsCherry? - Take a look at what they have to offer, you might make a friend!",
@@ -45,15 +46,47 @@ export default function Affiliates() {
                       <h2 className="p-2 bg-secondary text-black rounded-3xl"><strong>The AllsCherry Team!</strong></h2>
                       <h2 className="p-2 bg-secondary text-black rounded-xl text-center">Without the people listed below, AllsCherry wouldn&apos;t exist today! Each have their own unique skills and art form.<br/><br/>Check them out! You might make a friend!</h2>
                     </div>
+                    <div className="flex flex-col items-center justify-center text-sm font-semibold">
+                        <div className="flex felx-row items-center justify-center">
+                            <div className="flex flex-col items-center justify-center">
+                                <Image src={underConstruction} alt="Under Construction" width={400} height={400}/>
+                                <h2 className="bg-white rounded-3xl p-3 text-black">CEO / Founder - Eric Olcsvary</h2>
+                            </div>
+                            <div className="flex flex-col items-center justify-center">
+                                <Image src={underConstruction} alt="Under Construction" width={400} height={400}/>
+                                <h2 className="bg-white rounded-3xl p-3 text-black">CTO / WebDev - Hayden Zeller</h2>
+                            </div>
+                        </div>
+                        <div className="flex felx-row items-center justify-center">
+                            <div className="flex flex-col items-center justify-center">
+                                <Image src={underConstruction} alt="Under Construction" width={400} height={400}/>
+                                <h2 className="bg-white rounded-3xl p-3 text-black">Exec Assistant - Liam Fletcher</h2>
+                            </div>
+                            <div className="flex flex-col items-center justify-center">
+                                <Image src={underConstruction} alt="Under Construction" width={400} height={400}/>
+                                <h2 className="bg-white rounded-3xl p-3 text-black">Editor in Chief - Luke Hymers</h2>
+                            </div>
+                        </div>
+                        <div className="flex flex-col items-center justify-center w-1/2">
+                            <Image src={underConstruction} alt="Under Construction" width={400} height={400}/>
+                            <h2 className="bg-white rounded-3xl p-3 text-black">Printer - StudioComix Press</h2>
+                        </div>
+                    </div>
                </section>
-                <section className="felx flex-col justify-center items-center mt-24 text-center">
-                    <AffiliateList/>
-                </section>
                 <section className="flex flex-col justify-center mt-14 items-center">
                     <div className="bg-base-200 gap-5 rounded-3xl flex flex-col items-center justify-center mx-5 p-5 ">
                       <h2 className="p-2 bg-secondary text-black rounded-3xl"><strong>AllsCherry Affiliates / Artists!</strong></h2>
                       <h2 className="p-2 bg-secondary text-black rounded-xl text-center">AllsCherry works with many creatives! Below are those signed under the AllsCherry Label, or simply work with us in collaboration! Check out their work (So much talent!)</h2>
                     </div>
+                </section>
+                <section className="felx flex-col justify-center items-center mt-24 text-center">
+                    <AffiliateList/>
+                </section>
+                <section className="flex flex-col items-center justify-center mx-1 mt-14 gap-5">
+                    <Link href="/info/contact#contactForm" className="flex flex-row items-center justify-center gap-5 bg-white rounded-3xl px-5 py-1">
+                        <Image src={mailLogo} alt="Mail Logo" width={100} height={100} className="w-10"/>
+                        <h2 className="text-black">Email us here!</h2>
+                    </Link>
                 </section>
                 <Footer/>
             </section>
@@ -63,6 +96,7 @@ export default function Affiliates() {
                     <h1 className="text-2xl w-1/2 text-center">Oop, sorry! Desktop version is under construction, check back later!</h1>
                 </div>
             </section>
+
         </main>
     )
 }
