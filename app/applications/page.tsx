c0e2b261b20d3b297dd636ccb42ce3cf990fdeb9
Hayden Zeller <hayden@haydenzeller.com>
2024-09-07 11:27:02 -0400

    collab and application

--- app/applications/page.tsx
+++ app/applications/page.tsx
@@ -54,20 +54,20 @@ export default function Applications() {
     <main className="flex flex-col min-h-screen bg-base-100">
       <Header />
       {/* Mobile */}
-      <section className="flex flex-col items-center justify-center">
+      <section className="flex flex-col items-center justify-center lg:text-xl">
         <PageTitle title={"Looking to be published? Apply below!"} />
         <section className="flex flex-col items-center justify-center ">
-          <h2 className="text-md w-3/4 text-center border border-base-200 text-lg rounded-3xl mt-12 p-3">
+          <h2 className="text-md w-3/4 lg:w-auto lg:text-2xl text-center border border-base-200 text-lg rounded-3xl mt-12 p-3">
             Have an idea for a book?
           </h2>
-          <div className="flex flex-col text-black items-center justify-center mt-6 rounded-3xl bg-base-200 w-11/12 p-5">
-            <h2 className="bg-secondary rounded-3xl text-lg font-bold py-2 px-8">
+          <div className="flex lg:w-1/2 flex-col text-black items-center justify-center mt-6 rounded-3xl bg-base-200 w-11/12 p-5">
+            <h2 className="bg-secondary lg:w-full text-center lg:text-2xl rounded-3xl text-lg font-bold py-2 px-8">
               AllsCherry Publishing
             </h2>
-            <h2 className="bg-white rounded-xl font-bold text-md mt-3 py-2 px-8">
+            <h2 className="bg-white lg:w-3/4 rounded-xl font-bold text-md mt-3 py-2 px-8">
               Plan, Create, Produce, Print!
             </h2>
-            <p className="bg-secondary rounded-3xl py-2 px-3 mt-2">
+            <p className="bg-secondary rounded-3xl lg:w-3/4 py-2 px-3 mt-2">
               We help organize and map out your ideas from Step 0 all the way to
               a finished product, whether you&apos;re a writer, illustrator or
               both! Once we&apos;ve planned and completed your book creation, we
@@ -76,23 +76,23 @@ export default function Applications() {
           </div>
           <Link
             href="/applications/publication"
-            className="bg-primary text-black p-2 px-3 mt-6 border-b-accent border-b-4 rounded-full block w-34 mx-1 text-center active:mt-5 active:border-0"
+            className="bg-primary lg:text-2xl text-black p-2 px-3 mt-6 border-b-accent border-b-4 rounded-full block w-34 mx-1 text-center active:mt-5 active:border-0"
           >
             Join In!
           </Link>
         </section>
         <section className="flex flex-col items-center justify-center">
-          <h2 className="text-md w-3/4 text-center border border-base-200 text-lg rounded-3xl mt-12 p-3">
+          <h2 className="text-md w-3/4 lg:w-auto lg:text-2xl text-center border border-base-200 text-lg rounded-3xl mt-12 p-3">
             Not sure where to start?
           </h2>
-          <div className="flex flex-col text-black items-center justify-center mt-6 rounded-3xl bg-base-200 w-11/12 p-5">
-            <h2 className="bg-secondary rounded-3xl text-lg font-bold py-2 px-8">
+          <div className="flex flex-col lg:w-1/2 text-black items-center justify-center mt-6 rounded-3xl bg-base-200 w-11/12 p-5">
+            <h2 className="bg-secondary lg:w-full text-center lg:text-2xl rounded-3xl text-lg font-bold py-2 px-8">
               The Spooky Stubby Stories!
             </h2>
-            <h2 className="bg-white rounded-xl font-bold text-md mt-3 py-2 px-8">
+            <h2 className="bg-white lg:w-3/4 rounded-xl font-bold text-md mt-3 py-2 px-8">
               Work Together and Create a Short Story!
             </h2>
-            <p className="bg-secondary rounded-3xl py-2 px-3 mt-2">
+            <p className="bg-secondary lg:w-3/4 rounded-3xl py-2 px-3 mt-2">
               The Stubby Stories, a spooky postcard sized book for pairing up
               illustrators and writers. Each pairing creates a themed short
               story for the Fall Season! A simple way to get into book creation,
@@ -102,23 +102,23 @@ export default function Applications() {
           </div>
           <Link
             href="/applications/stubby-stories"
-            className="bg-primary text-black p-2 px-3 mt-6 border-b-accent border-b-4 rounded-full block w-34 mx-1 text-center active:mt-5 active:border-0"
+            className="bg-primary lg:text-2xl text-black p-2 px-3 mt-6 border-b-accent border-b-4 rounded-full block w-34 mx-1 text-center active:mt-5 active:border-0"
           >
             Join In!
           </Link>
         </section>
         <section className="flex flex-col items-center justify-center">
-          <h2 className="text-md w-3/4 text-center border border-base-200 text-lg rounded-3xl mt-12 p-3">
+          <h2 className="text-md w-3/4 lg:w-auto lg:text-2xl text-center border border-base-200 text-lg rounded-3xl mt-12 p-3">
             Have an idea for a short story? Publish as a group!
           </h2>
-          <div className="flex flex-col text-black items-center justify-center mt-6 rounded-3xl bg-base-200 w-11/12 p-5">
-            <h2 className="bg-secondary rounded-3xl text-lg font-bold py-2 px-8">
+          <div className="flex flex-col lg:w-1/2 text-black items-center justify-center mt-6 rounded-3xl bg-base-200 w-11/12 p-5">
+            <h2 className="bg-secondary lg:text-2xl lg:w-full text-center rounded-3xl text-lg font-bold py-2 px-8">
               The Annual Anthology
             </h2>
-            <h2 className="bg-white rounded-xl font-bold text-md mt-3 py-2 px-8">
+            <h2 className="bg-white rounded-xl lg:w-3/4 font-bold text-md mt-3 py-2 px-8">
               A Yearly Opportunity to &quot;Test the Water&quot;
             </h2>
-            <p className="bg-secondary rounded-3xl py-2 px-3 mt-2">
+            <p className="bg-secondary rounded-3xl lg:w-3/4 py-2 px-3 mt-2">
               Every year we gather artists big or small to publish a short story
               together within our Anthology Book! It&apos;s an easy way to get
               started in comics, gain attention through each other&apos;s work,
@@ -127,23 +127,23 @@ export default function Applications() {
           </div>
           <Link
             href="/applications/anthology"
-            className="bg-primary text-black p-2 px-3 mt-6 border-b-accent border-b-4 rounded-full block w-34 mx-1 text-center active:mt-5 active:border-0"
+            className="bg-primary lg:text-2xl text-black p-2 px-3 mt-6 border-b-accent border-b-4 rounded-full block w-34 mx-1 text-center active:mt-5 active:border-0"
           >
             Join In!
           </Link>
         </section>
         <section className="flex flex-col items-center justify-center">
-          <h2 className="text-md w-3/4 text-center border border-base-200 text-lg rounded-3xl mt-12 p-3">
+          <h2 className="text-md w-3/4 lg:w-auto lg:text-2xl text-center border border-base-200 text-lg rounded-3xl mt-12 p-3">
             Not sure where to start? Join us for a warm welcome!
           </h2>
-          <div className="flex flex-col text-black items-center justify-center mt-6 rounded-3xl bg-base-200 w-11/12 p-5">
-            <h2 className="bg-secondary rounded-3xl text-lg font-bold py-2 px-8">
+          <div className="flex flex-col lg:w-1/2 text-black items-center justify-center mt-6 rounded-3xl bg-base-200 w-11/12 p-5">
+            <h2 className="bg-secondary rounded-3xl lg:text-2xl text-center lg:w-full text-lg font-bold py-2 px-8">
               The Spring/Summer Stubby Stories
             </h2>
-            <h2 className="bg-white rounded-xl font-bold text-md mt-3 py-2 px-8">
+            <h2 className="bg-white rounded-xl lg:w-3/4 font-bold text-md mt-3 py-2 px-8">
               Work Together and Create a Short Story!
             </h2>
-            <p className="bg-secondary rounded-3xl py-2 px-3 mt-2">
+            <p className="bg-secondary rounded-3xl lg:w-3/4 py-2 px-3 mt-2">
               This Stubby Stories is a lighthearted postcard sized book for
               pairing up artists and writers. Each pairing creates a themed
               short story around the Warm Season! A simple way to get into book
@@ -153,12 +153,12 @@ export default function Applications() {
           </div>
           <Link
             href="/applications/stubby-stories"
-            className="bg-primary text-black p-2 px-3 mt-6 border-b-accent border-b-4 rounded-full block w-34 mx-1 text-center active:mt-5 active:border-0"
+            className="bg-primary lg:text-2xl text-black p-2 px-3 mt-6 border-b-accent border-b-4 rounded-full block w-34 mx-1 text-center active:mt-5 active:border-0"
           >
             Join In!
           </Link>
         </section>
-        <h2 className="text-md w-3/4 text-center border border-base-200 text-lg rounded-3xl mt-12 p-3">
+        <h2 className="text-md lg:w-auto lg:text-2xl w-3/4 text-center border border-base-200 text-lg rounded-3xl mt-12 p-3">
           If you want to learn more about past projects, check out our Anthology
           Page!
           <br />
--- app/publishing/collaboratives/page.tsx
+++ app/publishing/collaboratives/page.tsx
@@ -57,34 +57,34 @@ export default function Collaboratives() {
     <main className="flex flex-col min-h-screen bg-base-100">
       <Header />
       {/* Mobile */}
-      <section className="flex flex-col items-center justify-center">
+      <section className="flex flex-col items-center justify-center lg:text-xl">
         <PageTitle title={"Group projects have never been more fun!"} />
-        <h2 className="text-md w-3/4 text-center border border-base-200 text-lg rounded-3xl mt-12 p-3">
+        <h2 className="text-md lg:text-2xl w-3/4 lg:w-auto text-center border border-base-200 text-lg rounded-3xl mt-12 p-3">
           We welcome new and experienced artists to our two collaborative
           projects!
           <br />
           <br />
           The AllsCherry Anthology and Stubby Stories!
         </h2>
-        <div className="flex flex-col text-black items-center justify-center mt-6 rounded-3xl bg-base-200 w-11/12 p-5">
-          <h2 className="bg-secondary rounded-3xl text-lg font-bold py-2 px-8">
+        <div className="flex flex-col text-black items-center justify-center mt-6 lg:w-1/2 rounded-3xl bg-base-200 w-11/12 p-5">
+          <h2 className="bg-secondary lg:w-full text-center rounded-3xl text-lg lg:text-2xl font-bold py-2 px-8">
             The Annual Anthology
           </h2>
-          <p className="text-center text-sm opacity-40 mt-1">
+          <p className="text-center text-md opacity-40 mt-1">
             Runs from May - August
           </p>
-          <h2 className="bg-white rounded-xl font-bold text-md mt-3 py-2 px-8">
+          <h2 className="bg-white lg:w-3/4 text-center rounded-xl font-bold text-md mt-3 py-2 px-8">
             6 - 10 Comic Book Creators!
           </h2>
-          <p className="bg-secondary rounded-3xl py-2 px-3 mt-2 ">
+          <p className="bg-secondary rounded-3xl lg:w-3/4 lg:text-xl py-2 px-3 mt-2 ">
             We gather artists every year to create a book filled with original
             stories they want to share with the world. It&apos;s a great way to
             get into the comic industry and make connections!
           </p>
-          <h2 className="bg-white rounded-xl font-bold text-md mt-3 py-2 px-3">
+          <h2 className="bg-white rounded-xl font-bold lg:w-3/4 text-center text-md mt-3 py-2 px-3">
             Work together, or Create Your Own Story!
           </h2>
-          <p className="bg-secondary rounded-3xl py-2 px-3 mt-2">
+          <p className="bg-secondary rounded-3xl lg:w-3/4 py-2 px-3 mt-2">
             We accept your own stories, such as 2024&apos;s Creative Block by
             Kylah Heykoop, or Team Projects! In 2024, we had a story written by
             Eric, which was then illustrated by 4 other artists, all with their
@@ -99,41 +99,41 @@ export default function Collaboratives() {
               alt="Comics Publishing"
               width={700}
               height={700}
-              className="rounded-box w-1/2"
+              className="rounded-box w-1/2 lg:w-1/4"
             />
             <Image
               src={anthVol2}
               alt="Comics Publishing"
               width={700}
               height={700}
-              className="rounded-box w-1/2"
+              className="rounded-box w-1/2 lg:w-1/4"
             />
           </div>
           <Link
             href="/publishing/anthology"
-            className="bg-primary text-black p-2 px-3 mt-12 border-b-accent border-b-4 rounded-full block w-34 mx-1 text-center active:mt-5 active:border-0"
+            className="bg-primary lg:text-2xl text-black p-2 px-3 mt-12 border-b-accent border-b-4 rounded-full block w-34 mx-1 text-center active:mt-5 active:border-0"
           >
             Join In!
           </Link>
         </div>
-        <div className="flex flex-col text-black items-center justify-center mt-12 rounded-3xl bg-base-200 w-11/12 p-5">
-          <h2 className="bg-secondary rounded-3xl text-lg font-bold py-2 px-8">
+        <div className="flex flex-col text-black items-center lg:w-1/2 justify-center mt-12 rounded-3xl bg-base-200 w-11/12 p-5">
+          <h2 className="bg-secondary rounded-3xl lg:w-full lg:text-2xl text-center text-lg font-bold py-2 px-8">
             The Stubby Stories!
           </h2>
           <p className="text-center text-sm opacity-40 mt-1">
             Runs seasonally for 4 months
           </p>
-          <h2 className="bg-white rounded-xl font-bold text-md mt-6 py-2 px-8">
-            3 Writers and 3 Srtisits!
+          <h2 className="bg-white rounded-xl lg:w-3/4 text-center font-bold text-md mt-6 py-2 px-8">
+            3 Writers and 3 Artists!
           </h2>
-          <p className="bg-secondary rounded-3xl py-2 px-3 mt-2">
+          <p className="bg-secondary rounded-3xl lg:w-3/4 py-2 px-3 mt-2">
             Pairing up/working as a group, we create 3 short stories in our
             postcard sized book for the selected season, such as Halloween!
           </p>
-          <h2 className="bg-white rounded-xl font-bold text-md mt-6 py-2 px-3">
+          <h2 className="bg-white rounded-xl lg:w-3/4 text-center font-bold text-md mt-6 py-2 px-3">
             Selected Themes for Each Book!
           </h2>
-          <p className="bg-secondary rounded-3xl py-2 px-3 mt-2">
+          <p className="bg-secondary rounded-3xl lg:w-3/4 py-2 px-3 mt-2">
             To keep it interesting, our Stubby Stories are themed per book,
             having each short story tie in somehow together but each have their
             own unique style! In 2024, our Halloween released book was Fruit Pie
@@ -143,12 +143,12 @@ export default function Collaboratives() {
         <div className="flex flex-col items-center justify-center mt-12">
           <Link
             href="/publishing/anthology"
-            className="bg-primary text-black p-2 px-4 border-b-accent border-b-4 rounded-full block w-34 mx-1 text-center active:mt-5 active:border-0"
+            className="bg-primary lg:text-2xl text-black p-2 px-4 border-b-accent border-b-4 rounded-full block w-34 mx-1 text-center active:mt-5 active:border-0"
           >
             Join In!
           </Link>
         </div>
-        <h2 className="text-md w-3/4 text-center border border-base-200 text-lg rounded-3xl mt-12 p-3">
+        <h2 className="text-md lg:text-2xl lg:w-auto w-3/4 text-center border border-base-200 text-lg rounded-3xl mt-12 p-3">
           If you want to learn more about past projects, check out our Anthology
           Page!
           <br />
@@ -157,7 +157,7 @@ export default function Collaboratives() {
         </h2>
         <Link
           href="/projects/allscherry-anthology"
-          className="bg-primary text-black p-2 px-4 mt-4 border-b-accent border-b-4 rounded-full block w-34 mx-1 text-center active:mt-5 active:border-0"
+          className="bg-primary lg:text-2xl text-black p-2 px-4 mt-4 border-b-accent border-b-4 rounded-full block w-34 mx-1 text-center active:mt-5 active:border-0"
         >
           Projects
         </Link>
